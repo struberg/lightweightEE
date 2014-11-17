5fb43d82b6f189d9fc4728a8038dba0f0dae50d5
Mark Struberg <struberg@apache.org>
2014-11-17 15:24:22 +0100

    remove unused imports

--- backend/src/main/java/de/jaxenter/eesummit/caroline/backend/tools/EntityManagerProducer.java
+++ backend/src/main/java/de/jaxenter/eesummit/caroline/backend/tools/EntityManagerProducer.java
@@ -26,8 +26,6 @@ import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.PersistenceUnit;
 
 import org.apache.deltaspike.jpa.api.entitymanager.PersistenceUnitName;
 
